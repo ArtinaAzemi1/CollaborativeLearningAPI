6f3d579684997b46db37f4869a6c2d13927f693f
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2025-01-09 20:11:54 +0100

    Add project files.

---
+++ Models/Student.cs
@@ -0,0 +1,18 @@
+﻿namespace CollaborativeLearningAPI.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Surname { get; set; }
+
+        public DateTime? BirthDate { get; set; }
+
+        public string? Gender { get; set; }
+
+        public string? Email { get; set; }
+
+    }
+}
