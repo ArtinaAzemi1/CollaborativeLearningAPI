317f54c744585dbd30f05cc172b4fffb9fede7e7
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2025-01-14 04:11:06 +0100

    Ndryshim tek Coordinator

---
+++ Models/Coordinator.cs
@@ -0,0 +1,12 @@
+﻿namespace CollaborativeLearningAPI.Models
+{
+    public class Coordinator : Staff
+    {
+
+        public string FieldOfExpertise { get; set; }
+
+        public int YearsOfExperience { get; set; }
+
+        public string CertificationLevel { get; set; }
+    }
+}
