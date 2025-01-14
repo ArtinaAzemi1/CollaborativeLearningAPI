70baa69d17411eeddb7598f1317a38d30bc29380
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2025-01-14 04:28:01 +0100

    Shtimi i Schedule

---
+++ Models/Schedule.cs
@@ -0,0 +1,11 @@
+﻿namespace CollaborativeLearningAPI.Models
+{
+    public class Schedule
+    {
+        public int ScheduleId { get; set; }
+        public string Name { get; set; }
+        public string Day { get; set; } // Alternatively, you can use an enum for days of the week.
+        public TimeOnly Start { get; set; }
+        public TimeOnly End { get; set; }
+    }
+}
