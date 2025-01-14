608fcfbe71b77dfca6ec8656d09766bcae54243b
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2025-01-14 04:08:32 +0100

    Shtimi i Resource

---
+++ Models/Resource.cs
@@ -0,0 +1,11 @@
+﻿namespace CollaborativeLearningAPI.Models
+{
+    public class Resource
+    {
+        public int ResourceId { get; set; }
+
+        public string Type { get; set; }
+
+        public string Format { get; set; }
+    }
+}
