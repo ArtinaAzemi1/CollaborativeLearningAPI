c2b22297548fcba73cad7e2b59b15e7570f3b93f
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2025-01-14 04:04:19 +0100

    Ndryshim tek course, group dhe staff

---
+++ Models/Course.cs
@@ -0,0 +1,13 @@
+﻿namespace CollaborativeLearningAPI.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+
+        public string Name { get; set; }
+
+        public double ECTS { get; set; }
+
+        public string Semester { get; set; }
+    }
+}
---
+++ Models/Group.cs
@@ -0,0 +1,13 @@
+﻿namespace CollaborativeLearningAPI.Models
+{
+    public class Group
+    {
+        public int GroupId { get; set; }
+
+        public string Name { get; set; }
+
+        public int Year { get; set; }
+
+        public int Capacity { get; set; }
+    }
+}
