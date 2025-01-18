8d5da4fa86f50233df7ea017328a507d32b5694b
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2025-01-18 12:46:17 +0100

    Shtimi i Student Repository dhe interface

---
+++ Data/Repository/IStudentRepository.cs
@@ -0,0 +1,10 @@
+﻿using CollaborativeLearningAPI.Models;
+
+namespace CollaborativeLearningAPI.Data.Repository
+{
+    public interface IStudentRepository
+    {
+        Task<IEnumerable<Student>> GetStudents();
+        Task<Student> GetStudent(int id);
+    }
+}
---
+++ Data/Repository/StudentRepository.cs
@@ -0,0 +1,27 @@
+﻿using CollaborativeLearningAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CollaborativeLearningAPI.Data.Repository
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private readonly CollaborativeLearningDBContext _context;
+        public StudentRepository(CollaborativeLearningDBContext _context)
+        {
+            this._context = _context;
+        }
+
+        public async Task<IEnumerable<Student>> GetStudents()
+        {
+            return await _context.Students.ToListAsync();
+        }
+
+        public async Task<Student> GetStudent(int id)
+        {
+            return await _context.Students.FindAsync(id); 
+        }
+
+
+    }
+}
