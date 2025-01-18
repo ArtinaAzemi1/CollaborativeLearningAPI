3eb0cd1ec458d89c6519435f9b80fa924252e0f6
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2025-01-18 23:57:59 +0100

    Shtimi i repositoryt per group

---
+++ Data/Repository/IGroupRepository.cs
@@ -0,0 +1,15 @@
+﻿using CollaborativeLearningAPI.Models;
+
+namespace CollaborativeLearningAPI.Data.Repository
+{
+    public interface IGroupRepository
+    {
+        Task<IEnumerable<Group>> GetAllGroupsAsync();
+        Task<Group> GetGroupByIdAsync(int id);
+        Task<List<Student>> GetGroupStudentsAsync(int groupId);
+        public void AddGroupAsync(Group group);
+        public void UpdateGroupAsync(Group group);
+        public void DeleteGroupAsync(int groupId);
+        Task<bool> GroupExistsAsync(int id);
+    }
+}
