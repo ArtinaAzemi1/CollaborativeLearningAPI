f72fec47b789a6c7025b0c5d1ea4e614b4d9d074
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2025-01-20 19:27:36 +0100

    Shtimi i klases Feedback

---
+++ Models/Feedback.cs
@@ -0,0 +1,13 @@
+﻿namespace CollaborativeLearningAPI.Models
+{
+    public class Feedback
+    {
+        public int FeedbackId { get; set; }
+
+        public string Rating { get; set; }
+
+        public string Comment { get; set; }
+
+        public DateTime Date { get; set; }
+    }
+}
