607a7a8f3c67e3c5cc3514e4c1b930fcf4694f7e
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2025-01-20 20:41:39 +0100

    APIs per orarin jane shtuar

--- Controllers/ScheduleController.cs
+++ Controllers/ScheduleController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CollaborativeLearningAPI.Data;
+using CollaborativeLearningAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CollaborativeLearningAPI.Controllers
 {
@@ -7,5 +10,68 @@ namespace CollaborativeLearningAPI.Controllers
     [ApiController]
     public class ScheduleController : ControllerBase
     {
+        private readonly CollaborativeLearningDBContext _context;
+
+        public ScheduleController(CollaborativeLearningDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Schedule>>> GetSchedules()
+        {
+            return await _context.Schedule.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Schedule>> GetSchedule(int id)
+        {
+            var schedule = await _context.Schedule.FindAsync(id);
+
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            return schedule;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFeedback(int id, Schedule schedule)
+        {
+            if (id != schedule.ScheduleId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(schedule).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Schedule>> PostSchedule(Schedule schedule)
+        {
+            _context.Schedule.Add(schedule);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetSchedule", new { id = schedule.ScheduleId }, schedule);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteSchedule(int id)
+        {
+            var schedule = await _context.Schedule.FindAsync(id);
+            if (schedule == null)
+            {
+                return NotFound();
+            }
+
+            _context.Schedule.Remove(schedule);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
