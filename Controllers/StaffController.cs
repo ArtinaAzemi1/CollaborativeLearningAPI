3fc6b59f02a1bb2d9160ea28ddbfd9a326f8317f
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2025-01-20 21:13:46 +0100

    Shtimi i controller te staff si dhe fuksioneve perkatese

---
+++ Controllers/StaffController.cs
@@ -0,0 +1,97 @@
+﻿using CollaborativeLearningAPI.Data;
+using CollaborativeLearningAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollaborativeLearningAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StaffController : ControllerBase
+    {
+        private readonly CollaborativeLearningDBContext _context;
+
+        public StaffController(CollaborativeLearningDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Staff>>> GetStaff()
+        {
+            return await _context.Staff.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Staff>> GetStaffById(int id)
+        {
+            var staff = await _context.Staff.FindAsync(id);
+
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            return staff;
+        }
+
+        private bool StaffExists(int id)
+        {
+            return _context.Staff.Any(e => e.Id == id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Staff>> PostStaff(Staff staff)
+        {
+            _context.Staff.Add(staff);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetStaffById", new { id = staff.Id }, staff);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutStaff(int id, Staff staff)
+        {
+            if (id != staff.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(staff).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!StaffExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteStaff(int id)
+        {
+            var staff = await _context.Staff.FindAsync(id);
+            if (staff == null)
+            {
+                return NotFound();
+            }
+
+            _context.Staff.Remove(staff);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
