63c4c9721e975bf984876dc7644d763faa0ba211
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2025-01-20 20:38:04 +0100

    Funksionet/controller per resource

--- Controllers/ResourceController.cs
+++ Controllers/ResourceController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using CollaborativeLearningAPI.Data;
+using CollaborativeLearningAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace CollaborativeLearningAPI.Controllers
 {
@@ -7,5 +10,68 @@ namespace CollaborativeLearningAPI.Controllers
     [ApiController]
     public class ResourceController : ControllerBase
     {
+        private readonly CollaborativeLearningDBContext _context;
+
+        public ResourceController(CollaborativeLearningDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Resource>>> GetResources()
+        {
+            return await _context.Resources.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Resource>> GetResource(int id)
+        {
+            var resource = await _context.Resources.FindAsync(id);
+
+            if (resource == null)
+            {
+                return NotFound();
+            }
+
+            return resource;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutResource(int id, Resource resource)
+        {
+            if (id != resource.ResourceId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(resource).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Course>> PostResource(Resource resource)
+        {
+            _context.Resources.Add(resource);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetResource", new { id = resource.ResourceId }, resource);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteResource(int id)
+        {
+            var resource = await _context.Resources.FindAsync(id);
+            if (resource == null)
+            {
+                return NotFound();
+            }
+
+            _context.Resources.Remove(resource);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
