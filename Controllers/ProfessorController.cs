33a5dc93422cf4515dba87a41e1a9c9b2ded6dab
ArtinaAzemi1 <150926679+ArtinaAzemi1@users.noreply.github.com>
2025-01-20 23:21:22 +0100

    Controlleri per profesorin

---
+++ Controllers/ProfessorController.cs
@@ -0,0 +1,97 @@
+﻿using CollaborativeLearningAPI.Data;
+using CollaborativeLearningAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollaborativeLearningAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessorController : ControllerBase
+    {
+        private readonly CollaborativeLearningDBContext _context;
+
+        public ProfessorController(CollaborativeLearningDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Professor>>> GetProfessors()
+        {
+            return await _context.Professors.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Professor>> GetProfessorById(int id)
+        {
+            var professor = await _context.Professors.FindAsync(id);
+
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            return professor;
+        }
+
+        private bool ProfessorExists(int id)
+        {
+            return _context.Professors.Any(e => e.Id == id);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Professor>> PostProfessor(Professor professor)
+        {
+            _context.Professors.Add(professor);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProfessorById", new { id = professor.Id }, professor);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProfessor(int id, Professor professor)
+        {
+            if (id != professor.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(professor).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProfessorExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProfessor(int id)
+        {
+            var professor = await _context.Professors.FindAsync(id);
+            if (professor == null)
+            {
+                return NotFound();
+            }
+
+            _context.Professors.Remove(professor);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
